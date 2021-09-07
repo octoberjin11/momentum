4703324658150c3bf6df3063a7711a74c5117051
octoberjin11 <octoberjin11@gmail.com>
2021-09-08 00:19:22 +0900

    Deleting To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,22 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+//Todo list 삭제 버튼 이벤트
+function deleteTodo(event) {
+  const li = event.target.parentElement; //target은 클릭된 HTML element.
+  li.remove();
+}
+
+//Todo list 추가
 function paintTodo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
   toDoList.appendChild(li);
 }
 
