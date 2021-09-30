e9605c0d900c1dff9955979ade333658989d88d5
octoberjin11 <octoberjin11@gmail.com>
2021-09-30 17:39:25 +0900

    Create changeBg

---
+++ challenge/changebg/index.js
@@ -0,0 +1,32 @@
+const colors = [
+  "#ef5777",
+  "#575fcf",
+  "#4bcffa",
+  "#34e7e4",
+  "#0be881",
+  "#f53b57",
+  "#3c40c6",
+  "#0fbcf9",
+  "#00d8d6",
+  "#05c46b",
+  "#ffc048",
+  "#ffdd59",
+  "#ff5e57",
+  "#d2dae2",
+  "#485460",
+  "#ffa801",
+  "#ffd32a",
+  "#ff3f34",
+];
+
+const body = document.body;
+const colorBtn = document.querySelector("#color-btn");
+
+function randomBg() {
+  const randomColor1 = colors[Math.floor(Math.random() * colors.length)];
+  const randomColor2 = colors[Math.floor(Math.random() * colors.length)];
+
+  body.style.background = `linear-gradient(to right, ${randomColor1}, ${randomColor2})`;
+}
+
+colorBtn.addEventListener("click", randomBg);
